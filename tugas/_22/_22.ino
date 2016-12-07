711a9022de604a79a0bdefc489c7a271ec7ccff7
triita <triitaw@gmail.com>
2016-12-07 11:15:38 +0700

    Add files via upload

---
+++ tugas/_22/_22.ino
@@ -0,0 +1,22 @@
+int analogInPin = A1;
+int buzzer = 4;
+int sensorValue = 0;
+#include <Servo.h>
+Servo bipedal;
+
+void setup() {
+ bipedal.attach(5);
+ pinMode (buzzer, OUTPUT);
+}
+
+void loop() {
+ sensorValue = analogRead (analogInPin);
+ sensorValue = (5.0 * sensorValue * 100.0)/1024.0;
+  if (sensorValue >=125)
+  {
+    digitalWrite(buzzer, HIGH);
+    bipedal.write(90);
+  }
+delay(250);  
+}
+
